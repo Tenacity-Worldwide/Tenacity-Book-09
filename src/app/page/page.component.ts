f085f091739091199048b06af5e768a579fdb9a4
Joseph Edusei <jedusei99@gmail.com>
2022-05-31 01:07:50 +0000

    Initial commit

---
+++ src/app/page/page.component.ts
@@ -0,0 +1,99 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  NgZone,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { environment } from 'src/environments/environment';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PageComponent implements AfterViewInit {
+  loadState: 'loading' | 'success' | 'error' = 'loading';
+  zoom = 1;
+  @ViewChild('container') container!: ElementRef;
+
+  private _pageId: number = 1;
+  private resizeDebounceTimeout: any;
+
+  get pageId(): number {
+    return this._pageId;
+  }
+  @Input() set pageId(value: number) {
+    this._pageId = value;
+    this.loadContent();
+  }
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.updateZoom();
+    });
+  }
+
+  loadContent() {
+    this.loadState = 'loading';
+    fetch(`/assets/pages/${this.pageId}.html`)
+      .then((response) => response.text())
+      .then((html) => {
+        this.container.nativeElement.innerHTML = html;
+        this.loadState = 'success';
+
+        const mediaElements = $(this.container.nativeElement).find(
+          'audio, video'
+        );
+        mediaElements.on({
+          play: function (e: Event) {
+            mediaElements.each(function (_index, element: any) {
+              if (element != e.target) element.pause();
+            });
+          },
+        });
+
+        $(this.container.nativeElement)
+          .find('iframe')
+          .on('load', (event) => {
+            const iframe = event.target;
+            iframe.contentDocument?.body.style.setProperty(
+              'zoom',
+              this.zoom.toString()
+            );
+          });
+      })
+      .catch((err) => {
+        this.loadState = 'error';
+        console.error(err);
+      });
+  }
+
+  handleDoubleClick() {
+    if (!environment.production && this.loadState == 'success')
+      this.loadContent();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    clearTimeout(this.resizeDebounceTimeout);
+    this.resizeDebounceTimeout = setTimeout(() => {
+      this.updateZoom();
+    }, 1000);
+  }
+
+  updateZoom() {
+    if (this.container) {
+      const page: HTMLDivElement = this.container.nativeElement;
+      const pageWidth = 955.91;
+      this.zoom = Math.min(1, page.parentElement!.clientWidth / pageWidth);
+    }
+  }
+}
